bffab129734b2b1ed3f5aadb172291f049fbbb16
camilo-camargo <camilocamargo49@gmail.com>
2023-12-27 14:24:30 -0500

    Add ui react native and add .yarnrc with compatible config

--- packages/ui/ios/Leads1LLCTests/Leads1LLCTests.m
+++ packages/ui/ios/Leads1LLCTests/Leads1LLCTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface leads1llcTests : XCTestCase
+@interface Leads1LLCTests : XCTestCase
 
 @end
 
-@implementation leads1llcTests
+@implementation Leads1LLCTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
