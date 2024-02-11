93bb9eb7c78ec1274ed149ec6a16b670307b3560
camilo-camargo <camilocamargo49@gmail.com>
2024-02-10 22:14:39 -0500

    - Add nginx docker service config with certboot.

---
+++ config/nginx/docker/startup.sh
@@ -0,0 +1,10 @@
+echo "Installing Cerboot"
+apt update
+apt install certbot python3-certbot-nginx --yes
+
+echo "Running Cerboot"
+certbot --nginx --non-interactive --agree-tos --email camilocamargo49@gmail.com -d leads1llc.com -d www.leads1llc.com 
+
+echo "Running nginx"
+
+nginx -g "daemon off;"
