e95498d9b04ea7df3a358e1b30e1c3011f0d0727
camilo-camargo <camilocamargo49@gmail.com>
2024-02-18 10:49:59 -0500

    - Solve Content-Security-Policy content-src self

--- packages/core/config/server.ts
+++ packages/core/config/server.ts
@@ -4,6 +4,7 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('ADMIN_BACKEND_URL', ''),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
