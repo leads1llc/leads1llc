7c7e01f89206955ef7d47d2aa5b92f2515c39b0f
camilo-camargo <camilocamargo49@gmail.com>
2024-03-06 11:33:04 -0500

    - Add remix build to web startup

--- packages/web/startup.sh
+++ packages/web/startup.sh
@@ -10,6 +10,7 @@ yarn install
 
 if [ $ENV = "prod" ]; then
   echo "------------ PRODUCTION MODE ------------"
+  yarn build
   yarn start
 else
   echo "------------ DEVELOPMENT MODE ------------"
