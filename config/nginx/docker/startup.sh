993aab1bc4c175f73246131fbe4076c475155d27
camilo-camargo <camilocamargo49@gmail.com>
2024-03-06 11:39:58 -0500

    - Add app domain - Change ip address for network ips

--- config/nginx/docker/startup.sh
+++ config/nginx/docker/startup.sh
@@ -4,6 +4,7 @@ apt install certbot python3-certbot-nginx --yes
 
 echo "Running Cerboot"
 certbot --nginx --non-interactive --agree-tos --email camilocamargo49@gmail.com -d leads1llc.com -d www.leads1llc.com 
+certbot --nginx --non-interactive --agree-tos --email camilocamargo49@gmail.com -d app.leads1llc.com -d www.app.leads1llc.com 
 
 echo "Running nginx"
 
